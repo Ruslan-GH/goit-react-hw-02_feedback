4a74d11c2f3fe657114901ebc30087a367f3f5da
Ruslan-GH <kr.safetyok@gmail.com>
2023-02-02 22:07:29 +0200

    ready

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,62 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
+import React, {Component} from "react";
+import FeedbackOptions from "./FeedbackOptions";
+import Statistics from "../components/Statistics";
+import Section from "./Section";
+import Notification from './Notification/Notification'
+
+class App extends Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+
+  counterFeedback = (e) => {
+    this.setState(prevState => ({
+      [e]: prevState[e] + 1,
+    }));
+  };
+
+  countTotalFeedback = (e) => {
+    const total = Object.values(this.state).reduce((a,b)=>a+b,0);
+    return total
+  }
+
+  positivePercentageFeedbacks = () => {
+    const percentage = Math.round(
+      (this.state.good * 100)/this.countTotalFeedback(),
+      )
+    return percentage 
+  }
+
+  render() {
+
+    const array = ['good', 'neutral', 'bad'];
+    const totalFeedbacks = this.countTotalFeedback();
+    const positiveFeedbackPercentage =
+      this.state.good === 0 ? 0 : this.positivePercentageFeedbacks();
+    const title = "☕️ Cafe Expresso ☕️"
+    
+    return(
+      <Section title = {title}>
+          <FeedbackOptions 
+            options = {array} 
+            onLeaveFeedback = {this.counterFeedback}
+          />
+          {totalFeedbacks > 0 ? (
+          <Statistics 
+            good={this.state.good}
+            neutral={this.state.neutral}
+            bad={this.state.bad}
+            total={totalFeedbacks}
+            positivePercentage={positiveFeedbackPercentage}
+            />) 
+            : (
+            <Notification message = " 🤷‍♂️ There is no feedback 🤷‍♂️"/>)
+          }
+      </Section>
+    )
+  }
 };
+
+export default App;
